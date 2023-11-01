b0af874cfab96aa5f0ea1cef4fd2e1d076498e55
Hevi02 <108153417+Hevi02@users.noreply.github.com>
2023-11-01 17:54:14 -0300

    s

--- main.js
+++ main.js
@@ -27,3 +27,18 @@ function tocaSomToim(){
             document.querySelector('#som_tecla_toim').play();
         }
         document.querySelector('tecla_toim').onclick=tocaSomToim
+
+function tocaSomPsh(){
+        document.querySelector('#som_tecla_psh').play();
+}
+        document.querySelector('tecla_Psh').onclick=tocaSomPsh
+
+function tocaSomTic(){
+    document.querySelector('#som_tecla_tic').play();
+}
+    document.querySelector('tecla_Tic').onclick=tocaSomTic
+
+    function tocaSomTom(){
+        document.querySelector('#som_tecla_tom').play();
+    }
+        document.querySelector('tecla_Tom').onclick=tocaSomTom
